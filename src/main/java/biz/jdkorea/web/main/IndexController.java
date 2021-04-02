8ab83880bf711927e1c5b1dcbee61506ad40cc1d
JongwonKim0210 <71743704+JongwonKim0210@users.noreply.github.com>
2021-04-02 22:06:45 +0900

    default setting

---
+++ src/main/java/biz/jdkorea/web/main/IndexController.java
@@ -0,0 +1,22 @@
+package biz.jdkorea.web.main;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class IndexController {
+
+    private final Logger logger = LoggerFactory.getLogger(IndexController.class);
+
+    @RequestMapping(value = {"/", "/home"}, method = RequestMethod.GET)
+    public String getIndexPage() {
+        logger.info("request get Index Page...");
+
+        logger.info("making Index Page Complete!");
+        return "Index";
+    }
+
+}
