5170e8bed4d665c396dfc408444cefa986e3b16d
JongwonKim0210 <71743704+JongwonKim0210@users.noreply.github.com>
2021-04-03 20:06:35 +0900

    make Footer, index Page 70% complete

---
+++ src/main/resources/static/js/Common.js
@@ -0,0 +1,25 @@
+function moveBlog() {
+    var newTab = window.open("http://blog.naver.com/jdkorea1974", '_blank');
+    newTab.focus();
+}
+
+function moveCategory(index) {
+
+}
+
+function footerMoveCategory(index) {
+    switch (index) {
+        case 1 :    // 회사소개
+            break;
+        case 2 :    // 사업분야
+            break;
+        case 3 :    // 갤러리
+            break;
+        case 4 :    // 온라인문의
+            break;
+        case 5 :    // 고객센터
+            break;
+        case 6 :    // 로그인
+            break;
+    }
+}
