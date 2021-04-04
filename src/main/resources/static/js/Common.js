5d7a62b753d4122948f5dafdb782b31f1e491504
JongwonKim0210 <71743704+JongwonKim0210@users.noreply.github.com>
2021-04-04 20:06:12 +0900

    update mouseEffact(opacity option)

--- src/main/resources/static/js/Common.js
+++ src/main/resources/static/js/Common.js
@@ -4,20 +4,29 @@ function moveBlog() {
 }
 
 function moveCategory(index) {
+    var targetPage;
     switch (index) {
         case 1 :    // index page
+            targetPage = "/home";
             break;
         case 2 :    // about
+            targetPage = "/about";
             break;
         case 3 :    // business
+            targetPage = "/business";
             break;
         case 4 :    // gallery
+            targetPage = "/gallery";
             break;
         case 5 :    // online
+            targetPage = "/online";
             break;
         case 6 :    // customer
+            targetPage = "/customer";
             break;
     }
+
+    location.href = targetPage;
 }
 
 function bannerClick() {
@@ -25,18 +34,27 @@ function bannerClick() {
 }
 
 function footerMoveCategory(index) {
+    var targetPage;
     switch (index) {
         case 1 :    // 회사소개
+            targetPage = "/about";
             break;
         case 2 :    // 사업분야
+            targetPage = "/business";
             break;
         case 3 :    // 갤러리
+            targetPage = "/gallery";
             break;
         case 4 :    // 온라인문의
+            targetPage = "/online";
             break;
         case 5 :    // 고객센터
+            targetPage = "/customer";
             break;
         case 6 :    // 로그인
+            targetPage = "/login";
             break;
     }
+
+    location.href = targetPage;
 }
--- src/main/resources/static/js/mouseEffact.js
+++ src/main/resources/static/js/mouseEffact.js
@@ -5,10 +5,10 @@ $(document).ready(function () {
 // index page, banner div opacity option
 function setIndexOpacity() {
     $(document).on("mouseover", "div#index_banner_area div", function () {
-        $(this).animate({opacity:"0.8"})
+        $(this).animate({opacity:"0.8"}, 200);
     });
     $(document).on("mouseout", "div#index_banner_area div", function () {
-        $(this).animate({opacity:"1"})
+        $(this).animate({opacity:"1"}, 200);
     });
 }
 
